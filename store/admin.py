1f73743d2d4cdaa72786f5ec5f330cafd7aae2b3
Pradeep Kumar <4gpradeep@gmail.com>
2021-10-10 16:15:59 +0530

    Filtering List Page:

--- store/admin.py
+++ store/admin.py
@@ -1,14 +1,29 @@
 from django.contrib import admin
 from django.db.models.aggregates import Count
+from django.db.models.query import QuerySet
 from django.urls import reverse
 from django.utils.html import format_html,urlencode
 from . import models
 # Register your models here.
 
+class InventoryFilter(admin.SimpleListFilter):
+    title='inventory'
+    parameter_name='inventory'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<10','Low'),
+
+        ]
+    def queryset(self, request, queryset:QuerySet):
+        if self.value()=='<10':
+            return queryset.filter(inventory__lt=10)
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display=['title','unit_price','inventory_status','collection']
     list_editable = ['unit_price']
+    list_filter=['collection','last_update',InventoryFilter]
     list_per_page=10
     # list_select_related=['collection']
 
@@ -26,8 +41,9 @@ class ProductAdmin(admin.ModelAdmin):
 class CustomerAdmin(admin.ModelAdmin):
     list_display=['first_name','last_name','membership','orders_count']
     list_editable=['membership']
-    ordering=['first_name','last_name']
     list_per_page=10
+    ordering=['first_name','last_name']
+    search_fields=['first_name__istartswith','last_name__istartswith']
 
     @admin.display(ordering='orders_count')
     def orders_count(self,Customer):
@@ -37,7 +53,7 @@ class CustomerAdmin(admin.ModelAdmin):
             'customer_id':str(Customer.id)
         }))
         return format_html('<a href = "{}">{}</a>',url,Customer.orders_count)
-        
+
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(
             orders_count = Count('order')
