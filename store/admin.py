2e4601d87a0412e926c47352ef58a7df0f5c815a
Pradeep Kumar <4gpradeep@gmail.com>
2021-10-10 16:49:22 +0530

    customizing forms

--- store/admin.py
+++ store/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib import admin,messages
 from django.db.models.aggregates import Count
 from django.db.models.query import QuerySet
 from django.urls import reverse
@@ -21,6 +21,13 @@ class InventoryFilter(admin.SimpleListFilter):
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
+    # fields=['title','slug']
+    # exclude=['promotions']
+    autocomplete_fields=['collection']
+    prepopulated_fields={
+        'slug':['title']
+    }
+    actions=['clear_inventory']
     list_display=['title','unit_price','inventory_status','collection']
     list_editable = ['unit_price']
     list_filter=['collection','last_update',InventoryFilter]
@@ -36,7 +43,14 @@ class ProductAdmin(admin.ModelAdmin):
             return 'Low'
         else:
             return 'Ok'
-
+    @admin.action(description='Clear Inventory')
+    def clear_inventory(self,request,queryset):
+        updated_count = queryset.update(inventory= 0)
+        self.message_user(
+            request,
+            f'{updated_count} products were updated successfully',
+            messages.SUCCESS
+        )
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display=['first_name','last_name','membership','orders_count']
@@ -61,6 +75,7 @@ class CustomerAdmin(admin.ModelAdmin):
 
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
+    autocomplete_fields=['customer']
     list_display=['id','placed_at','customer']
     list_per_page=10
     list_select_related=['customer']
@@ -69,7 +84,7 @@ class OrderAdmin(admin.ModelAdmin):
 class CollectionAdmin(admin.ModelAdmin):
     list_display=['title','products_count']
     list_per_page=10
-
+    search_fields=['title']
     @admin.display(ordering='products_count')
     def products_count(self,Collection):
         # reverse('admin:app_model_page')
