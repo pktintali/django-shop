d0cca86772e38783781b6f9841890792d58c15f7
Pradeep Kumar <4gpradeep@gmail.com>
2021-10-10 08:46:39 +0530

    cursor execute query

--- playground/views.py
+++ playground/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
-from django.db import transaction
+from django.db import transaction,connection
 from django.contrib.contenttypes.models import ContentType
 from store.models import Collection, Order, OrderItem, Product
 from tags.models import TaggedItem
 
 
 def say_hello(request):
-    queryset = Product.objects.raw(
-        'SELECT * FROM store_product'
-    )
-
+    with connection.cursor() as cursor:
+        cursor.execute()
+    
     return render(
         request, "hello.html", {"name": "Pradeep Kumar","result":list(queryset)}
     )
