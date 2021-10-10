b7f582b3b383c1036b04b5e2e6f5ae90f876a5e3
Pradeep Kumar <4gpradeep@gmail.com>
2021-10-10 08:21:36 +0530

    Transcations

--- playground/views.py
+++ playground/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render
+from django.db import transaction
 from django.contrib.contenttypes.models import ContentType
-from store.models import Collection, Product
+from store.models import Collection, Order, OrderItem, Product
 from tags.models import TaggedItem
 
 
+#Use decorator for entire function 
+#@transaction.atomic()
 def say_hello(request):
-    # collection = Collection.objects.get(pk=11)
-    # #Update Carefully
-    # collection.featured_product=None
-    # collection.save()
+    #...
+    #with for a pert of function
+    with transaction.atomic():
+        order = Order()
+        order.customer_id = 1
+        order.save()
 
-    Collection.objects.filter(pk=11).update(featured_product=None)
+
+        item = OrderItem()
+        item.order=order
+        item.product_id = -1
+        item.quantity=1
+        item.unit_price=10
+        item.save()
+
     return render(
         request, "hello.html", {"name": "Pradeep Kumar",}
     )
